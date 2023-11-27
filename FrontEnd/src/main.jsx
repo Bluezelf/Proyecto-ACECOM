95456a30b85c753f95b8be8fbe0b9f3acb49662f
José Bustos <bustosttitojosefabricio@gmail.com>
2023-11-26 21:22:10 -0500

    Update main.jsx

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -10,6 +10,8 @@ const socket = io(serverUrl);
 const SensorData = () => {
     const [data, setData] = useState(null);
     const [data_2, setData_2] = useState(null);
+    const [data_3, setData_3] = useState(null);
+    const [data_4, setData_4] = useState(null);
 
     useEffect(() => {
         // No necesitas manejar la conexión y desconexión aquí.
@@ -26,6 +28,16 @@ const SensorData = () => {
             setData_2(receivedData); // Actualiza el estado con los datos recibidos
         });
 
+        socket.on('1/humidity', (receivedData) => {
+            console.log('Datos recibidos:', receivedData);
+            setData_3(receivedData); // Actualiza el estado con los datos recibidos
+        });
+
+        socket.on('1/date', (receivedData) => {
+            console.log('Datos recibidos:', receivedData);
+            setData_4(receivedData); // Actualiza el estado con los datos recibidos
+        });
+
         return () => {
             socket.off('1/datos'); // Desvincular el evento al desmontar el componente
         };
@@ -38,6 +50,8 @@ const SensorData = () => {
                     {/* Mostrar los datos en tu componente según sea necesario */}
                     <p>Temperatura: {data}</p>
                     <p>Air Quality: {data_2}</p>
+                    <p>Humidity: {data_3}</p>
+                    <p>Date: {data_4}</p>
                     {/* Otros campos de datos */}
                 </div>
             )}
